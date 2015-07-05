641853c9693423e1227136ddc2181cef28cf31a1
Łukasz Biedak <l.biedak@wp.pl>
2015-07-05 17:23:13 +0100

    Adding counter to calculate inversions

--- MergeSort.py
+++ MergeSort.py
@@ -7,6 +7,9 @@ from random import randint
 
 class MergeSorter:
     
+    def __init__(self):
+        self.it = 0
+    
     def sort(self, x):
         size = len(x)
         if(size>1):
@@ -23,7 +26,7 @@ class MergeSorter:
         j=0
         c = []
         for k in range(len(a)+len(b)):
-            if(a[i]<b[j]):
+            if(a[i]<=b[j]):
                 c.append(a[i])
                 i+=1
                 if(i == len(a)):
@@ -31,13 +34,14 @@ class MergeSorter:
                     break
                 
             else:
+                self.it+=len(a)-i
                 c.append(b[j])
                 j+=1
                 if(j == len(b)):
                     c.extend(a[i:])
                     break
                 
-        print(c)         
+                 
         return c
 
 
@@ -46,6 +50,10 @@ a = []
 for i in range(1000):
     a.append(randint(0, 1000))
 
+a = [1,3,5, 7,2,4,6,1,8,0]
+
 merger = MergeSorter()
 x = merger.sort(a)
+print (a)
 print (x)
+print (merger.it)
